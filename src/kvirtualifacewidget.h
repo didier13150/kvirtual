cbe08665fc7d867779845f7a3505c0f4b2b804a9
Didier Fabert <didier.fabert@gmail.com>
2012-05-07 11:34:54 +0200

    Add iface widget

---
+++ src/kvirtualifacewidget.h
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ *      ____              ____    _       _   _                              *
+ *     /# /_\_           |  _ \  (_)   __| | (_)   ___   _ __                *
+ *    |  |/o\o\          | | | | | |  / _` | | |  / _ \ | '__|               *
+ *    |  \\_/_/          | |_| | | | | (_| | | | |  __/ | |                  *
+ *   / |_   |            |____/  |_|  \__,_| |_|  \___| |_|                  *
+ *  |  ||\_ ~|                                                               *
+ *  |  ||| \/                                                                *
+ *  |  |||       Project : KVirtual : a KDE4 GUI frontend for KVM            *
+ *  \//  |                                                                   *
+ *   ||  |       Developper : Didier FABERT <didier.fabert@gmail.com>        *
+ *   ||_  \      Date : 2012, April                                          *
+ *   \_|  o|                                             ,__,                *
+ *    \___/      Copyright (C) 2012 by didier fabert     (oo)____            *
+ *     ||||__                                            (__)    )\          *
+ *     (___)_)   File : kvirtualifacewidget.h               ||--|| *         *
+ *                                                                           *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .          *
+ *****************************************************************************/
+
+
+#ifndef KVIRTUALIFACEWIDGET_H
+#define KVIRTUALIFACEWIDGET_H
+
+#include "ui_kvirtualiface_widget.h"
+
+class KVirtualIfaceWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    KVirtualIfaceWidget( QWidget *parent = 0 );
+    virtual ~KVirtualIfaceWidget();
+
+protected:
+    Ui::KVirtualIfaceView ui_widget;
+};
+
+#endif // KVIRTUALIFACEWIDGET_H
+// kate: indent-mode cstyle; replace-tabs off; tab-width 4;
