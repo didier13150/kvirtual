3723ec2a030498daffd587afee216c5b7529fbab
Didier Fabert <didier.fabert@gmail.com>
2012-05-09 10:28:18 +0200

    BUGFIX wrong path for pot file in i18n script

--- src/Messages.sh
+++ src/Messages.sh
@@ -4,8 +4,8 @@ BASEDIR="../"	# root of translatable sources
 PROJECT="kvirtual"	# project name
 BUGADDR="https://github.com/didier13150/kvirtual/issues"	# MSGID-Bugs
 WDIR=`pwd`		# working dir
- 
- 
+
+
 echo "Preparing rc files"
 # we use simple sorting to make sure the lines do not jump around too much from system to system
 find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > rcfiles.list
@@ -14,8 +14,8 @@ xargs --arg-file=rcfiles.list extractrc > rc.cpp
 echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> rc.cpp
 echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> rc.cpp
 echo "Done preparing rc files"
- 
- 
+
+
 echo "Extracting messages"
 # see above on sorting
 find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > infiles.list
@@ -26,18 +26,18 @@ xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki1
 	--msgid-bugs-address="${BUGADDR}" \
 	--files-from=infiles.list -D . -o ../po/${PROJECT}.pot || { echo "error while calling xgettext. aborting."; exit 1; }
 echo "Done extracting messages"
- 
- 
+
+
 echo "Merging translations"
 catalogs=`find ../po/ -name '*.po'`
 for cat in $catalogs; do
   echo $cat
-  msgmerge -o $cat.new $cat ${PROJECT}.pot
+  msgmerge -o $cat.new $cat ../po/${PROJECT}.pot
   mv $cat.new $cat
 done
 echo "Done merging translations"
- 
- 
+
+
 echo "Cleaning up"
 rm rcfiles.list
 rm infiles.list
